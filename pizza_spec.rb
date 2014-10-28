c575ca4802d2f2325a9bd602c747ce48f798eaa2
Abigail Tuckner & Matthew Meyers <pair+abbiecat.marinatedpork@devbootcamp.com>
2014-10-28 10:26:42 -0500

    pizza tests one and two

--- pizza_spec.rb
+++ pizza_spec.rb
@@ -1,7 +1,20 @@
 require 'rspec'
 
 describe 'Pizza' do
+  let(:pizza) {Pizza.new("Pizza de Giovanni")}
   it 'is a pizza' do
-    expect(Pizza.new).to be_a Pizza
+    expect(pizza).to be_a Pizza
+  end
+
+  it 'has a name' do
+    expect(pizza.name).to be_a String
+  end
+
+  it 'has a description' do
+    pizza.should respond_to(:description)
+  end
+
+  it 'has a time_baked' do
+    pizza.should respond_to(:time_baked)
   end
 end
