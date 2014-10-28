9657fc745841b5a8362ef246cd8d53768ad4f9ba
Christian Stanfield <christian.stanfield@gmail.com>
2014-10-28 10:34:29 -0500

    passed all 4 tests

--- pizza.rb
+++ pizza.rb
@@ -1,5 +1,8 @@
 class Pizza
-  def initialize
-
+  attr_reader :name, :description, :time_baked
+  def initialize(name)
+    @name = name
+    @description = nil
+    @time_baked = nil
   end
 end
--- pizza_spec.rb
+++ pizza_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require_relative 'pizza'
 
 describe 'Pizza' do
   let(:pizza) {Pizza.new("Pizza de Giovanni")}
